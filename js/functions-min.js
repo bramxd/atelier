bffe9855436383a64af2c09df1d0d2eb1c4214a0
bramxd <mohamed.rahmani@esprit.tn>
2023-12-13 23:33:08 +0100

    portfolio

---
+++ js/functions-min.js
@@ -0,0 +1,12 @@
+var x=parseInt(prompt("A quel point vous voulez voir mon cv?")); 
+            if(x>=1 && x<=3){
+               alert("Tu changeras d'avis plus tard");
+            }else if(x>=4 && x<=7){
+               alert("Tu ne regretteras jamais");
+            }else if(x>=8 && x<=10){
+              alert("Excellent, let's gooo!");
+            }else{
+               alert ("Vous devez entre un chiffres entre 1 et 10");
+            }
+
+
