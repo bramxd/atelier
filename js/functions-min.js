55324e3fbe00622f1ad323d2ac0be181571ea636
bramxd <mohamed.rahmani@esprit.tn>
2023-12-17 12:49:05 +0100

    portfolio

--- js/functions-min.js
+++ js/functions-min.js
@@ -1,4 +1,4 @@
-var x=parseInt(prompt("A quel point vous voulez voir mon cv?")); 
+var x=parseInt(prompt("A quel point vous voulez voir mon cv?(entrez un chiffre entre 1 et 10)")); 
             if(x>=1 && x<=3){
                alert("Tu changeras d'avis plus tard");
             }else if(x>=4 && x<=7){
@@ -10,3 +10,65 @@ var x=parseInt(prompt("A quel point vous voulez voir mon cv?"));
             }
 
 
+document.addEventListener('DOMContentLoaded', function () {
+   const profileImages = document.querySelectorAll('.section__img-container img');
+ 
+   profileImages.forEach(image => {
+     image.style.opacity = '0';
+     image.style.transform = 'scale(0.5)';
+     image.style.transition = 'opacity 1s, transform 1s';
+ 
+     setTimeout(() => {
+       image.style.opacity = '1';
+       image.style.transform = 'scale(1)';
+     }, 500);
+   });
+ });
+ 
+ 
+ document.addEventListener('DOMContentLoaded', function () {
+  const profileImages = document.querySelectorAll('.section__img-container img');
+
+  profileImages.forEach(image => {
+    image.addEventListener('mouseenter', function () {
+      this.style.transform = 'scale(1.1)';
+    });
+
+    image.addEventListener('mouseleave', function () {
+      this.style.transform = 'scale(1)';
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.btn');
+
+  buttons.forEach(button => {
+    button.style.transition = 'transform 0.3s ease';
+
+    button.addEventListener('mouseenter', function () {
+      this.style.transform = 'scale(1.1)';
+    });
+
+    button.addEventListener('mouseleave', function () {
+      this.style.transform = 'scale(1)';
+    });
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section');
+
+  sections.forEach(section => {
+    section.style.transition = 'transform 0.3s ease';
+
+    section.addEventListener('mouseenter', function () {
+      this.style.transform = 'scale(1.05)';
+    });
+
+    section.addEventListener('mouseleave', function () {
+      this.style.transform = 'scale(1)';
+    });
+  });
+});
